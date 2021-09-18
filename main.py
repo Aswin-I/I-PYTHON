a93b989bc2f7b42731c74a961cb4834b185572e9
Aswin-I <79042899+Aswin-I@users.noreply.github.com>
2021-09-18 12:55:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+film = "Manichitrathazhu"
+actor = "Mohanlal"
+actress = "Shobana"
+director = "Fazil"
+year_of_release = 1993
+print(f"my fav movie is {film},\n the lead actor and actress were {actor} and {actress},\n the film was realeased in {year_of_release}")
